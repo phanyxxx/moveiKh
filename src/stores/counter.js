9d7280221a3a86f025660698db8b2f16ae742794
Nang Phany <your-email@example.com>
2026-07-28 11:03:38 +0700

    hello

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -21,68 +21,6 @@ export const useListMenuStore = defineStore("listMenu", {
                 active: false
             },
         ],
-        movieList:[
-            {
-                id: 1,
-                name: "Hang Knung Vong Chorchork",
-                image: "m1.jpg",
-                active: false
-            },
-            {
-                id: 2,
-                name: "Nisai Chivit Nery PhKu Phkang",
-                image: "m2.jpg",
-                active: false
-            },
-            {
-                id: 3,
-                name: "Som leng Damtri Chlong Vealea",
-                image: "m3.jpg",
-                active: false
-            },
-            {
-                id: 4,
-                name: "Smorphumi Nak Klahan",
-                image: "m4.jpg",
-                active: false
-            },
-            {
-                id: 5,
-                name: "Piphop kun Nak klahan",
-                image: "m5.jpg",
-                active: false
-            },
-            {
-                id: 6,
-                name: "Krom Brotebatka Songkrous Chivit",
-                image: "m6.jpg",
-                active: false
-            },
-            {
-                id: 7,
-                name: "Damnok Cham Reach Vong Atkombang",
-                image: "m7.jpg",
-                active: false
-            },
-            {
-                id: 8,
-                name: "Onteak Sdach Chorchork Lolourng Sne Preas Neang",
-                image: "m8.jpg",
-                active: false
-            },
-            {
-                id: 9,
-                name: "Athkombang Samai Thang I",
-                image: "m9.jpg",
-                active: false
-            },
-            {
-                id: 10,
-                name: "Kompul Metep Srey Ler Samorphum",
-                image: "m10.jpg",
-                active: false
-            },
-        ]
     }),
     getters: {
         getLists(){
